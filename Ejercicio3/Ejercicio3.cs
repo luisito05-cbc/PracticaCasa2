9d8b4775e94c33aadbcd0cacc9847c975b5abea1
luis fernando viera chagas <luisfernandovierachagas@192.168.1.45>
2025-03-12 23:23:48 -0300

    Initial commit

---
+++ Ejercicio3/Ejercicio3.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio3
+{
+    internal class Ej3
+
+    {
+        static void Main(string[] args)
+        {
+            /*Ejercicio 3.
+                Solicitar dos números (controlar que el primer número sea menor al segundo) y muestre todos
+                los números entre los valores ingresados que sean pares. Para salir se debe ingresar 0.*/
+        
+            Console.WriteLine("Inregar primer numero");
+            int numer1= int.Parse(Console.ReadLine());
+        
+            Console.WriteLine("Ingresar segundo numero");
+
+            int numer2 = int.Parse(Console.ReadLine());
+        
+        
+
+            if (numer1 > numer2)
+            {
+                Console.WriteLine("El primer numero debe ser menor al segundo");
+
+                for (int i = numer1; i <= numer2; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+
+
+        //No me imprime el rango de numero de i
+        }
+    }
+
+}
+
---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ej4
+{
+    internal class Ej4
+    {
+        static void Main(string[] args)
+        {
+            /*Solicitar dos números y un valor, indicar si el valor está comprendido entre los números*/
+            
+            //SE INGRESA EL PRIMER NUMERO
+            Console.WriteLine("Ingrese el primer numero");
+            int num1 = int.Parse(Console.ReadLine());
+            
+            //SE INGRESA EL SEGUNDO NUMERO
+            Console.WriteLine("Ingrese el segundo numero");
+            int num2 = int.Parse(Console.ReadLine());
+            
+            //SE INGRESA EL VALOR NUMERO
+            Console.WriteLine("Inresar un valor");
+            int valor = int.Parse(Console.ReadLine());
+
+            if (valor > num1 && valor < num2)
+            {
+                Console.WriteLine($"el numero {valor } esta dentro de los rangos comprendidos de {num1} y {num2}");
+            }
+
+            Console.ReadKey();
+
+        }
+        
+        
+    }
+
+}
+
